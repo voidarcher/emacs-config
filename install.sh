43156f102c34075e53878637a7ace19e18b6798e
ban <bisshoy@protonmail.com>
2025-05-22 12:11:16 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Bisshoy Anwar
+# Rune Emacs Install Script Version 0.1
+
+EMACSDIR=".emacs.d"
+
+echo "Hello, and welcome to the Rune Emacs Installer!"
+
+# Check for extant ~/.emacs.d, and create one if none exists 
+# If one does already exist, then create backup and wipe the
+# currently existing one 
+if [ -d ~/$EMACSDIR ]; then
+    echo "~/$EMACSDIR exists; creating a backup and cleaning... "
+    cp -R ~/$EMACSDIR ~/$EMACSDIR.backup/
+    rm -rf ~/$EMACSDIR/*
+    echo "The contents of your old $EMACSDIR can be found at ~/$EMACSDIR.backup"
+else
+    echo "No ~/$EMACSDIR exists; creating now..."
+    mkdir ~/$EMACSDIR/
+    echo "~/$EMACSDIR created!"
+fi
+
+# Change into the ~/.emacs.d directory 
+cd ~/$EMACSDIR
+
+# Clone GitHub repo into ~/.emacs.d directory 
+echo "----------------------------------------------------"
+git clone https://github.com/voidarcher/RuneMacs.git ~/$EMACSDIR/
+echo "----------------------------------------------------"
+
+# Cleanup
+echo "Cleaning up ~/$EMACSDIR..."
+rm -rf ./.git LICENSE README.md
+
+# Welcome message 
+echo ""
+echo "Welcome to RUNE EMACS!"
+echo "Now, please open Emacs and allow the syncing to occur!"
